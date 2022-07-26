d8b51d16b0f8d458becbaa7f7c0b5bb08591f385
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-26 15:30:03 -0400

    added dependencies for Apollo client, connecting client in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home';
+// import apollo client to connect to back end
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+import Home from "./pages/Home";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
+    <div className="flex-column justify-flex-start min-100-vh">
       <Header />
-      <div className='container'>
+      <div className="container">
         <Home />
       </div>
       <Footer />
