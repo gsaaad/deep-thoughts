4d18ff750b5dbe44e5abb52fef198e566a8df954
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-10-07 10:13:31 -0400

    added single thought form

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-// import apollo client to connect to back end
-
 import {
   ApolloProvider,
   ApolloClient,
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
+
+import { Link } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+
+// import apollo client to connect to back end
 
 import Home from "./pages/Home";
 
@@ -27,13 +35,25 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile">
+                <Route path=":username" element={<Profile />} />
+                <Route path="" element={<Profile />} />
+              </Route>
+              <Route path="/thought/:id" element={<SingleThought />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
